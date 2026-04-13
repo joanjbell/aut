59e949fb8a93d575cd4ace64ac80fdf953f5ce4d
joanjbell <joanjbellauthor@gmail.com>
2026-04-12 23:00:19 -0500

    books on homepage

--- scripts/update-goodreads.js
+++ scripts/update-goodreads.js
@@ -8,7 +8,6 @@ function fetchHTML(url) {
   return new Promise((resolve, reject) => {
     https.get(url, (res) => {
       let data = '';
-
       res.on('data', chunk => data += chunk);
       res.on('end', () => resolve(data));
       res.on('error', reject);
@@ -22,25 +21,26 @@ async function scrape() {
 
   const books = [];
 
-  $('.bookTitle').each((i, el) => {
-    const title = $(el).text().trim();
+  // ✅ STRICT SCOPING TO AUTHOR BOOK TABLE
+  $('#booksByAuthor tr').each((i, el) => {
+    const titleEl = $(el).find('.bookTitle').first();
+    const title = titleEl.text().trim();
 
-    const cover = $(el)
-      .closest('tr')
-      .find('img')
-      .attr('src');
+    const cover = $(el).find('img').attr('src');
 
-    if (title) {
+    const link = titleEl.attr('href');
+
+    if (title && cover) {
       books.push({
         title,
         type: "Book",
-        description: "From Goodreads",
+        description: "Published work",
         cover,
-        link: "#"
+        link: link ? `https://www.goodreads.com${link}` : "#"
       });
     }
   });
-
+  books = [...new Map(books.map(b => [b.title, b])).values()];
   fs.writeFileSync('./data/books.json', JSON.stringify(books, null, 2));
   console.log('Books updated!');
 }
